f2810211ba19fe671073002b1bdebed65f52ee17
Devi T <114129079+DeviThanikachalam@users.noreply.github.com>
2023-06-27 19:20:32 +0530

    Decision tree classification.py

--- Decision tree classification.py
+++ Decision tree classification.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 import pandas as pd
 from sklearn import tree
 from sklearn.model_selection import train_test_split
@@ -17,16 +11,3 @@ model.fit(x_train,y_train)
 print('Score: ',model.score(x_test,y_test))
 print('Predicted as: ',model.predict([[4.4,2.9,1.4,0.2]]))
 tree.plot_tree(model)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
